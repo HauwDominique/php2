929b74698af9106d9ddb4da8bb3403fde3f2640a
hauw <hauw.dominique@gmail.com>
2018-10-23 17:19:15 +0200

    exo PDO pizza

--- 12-pdo/index.php
+++ 12-pdo/index.php
@@ -1,6 +1,50 @@
 <?php
 
+// try {
+//     $db = new PDO('mysql:host=localhost; dbname=pizzastore', 'root', ''
+//     [ PDO::ATTR_ERRMODE => PDO:: ERRMODE_WARNING, //active les erreurs SQL
+//     ]); 
+// } catch(Exception $e) {
+//     echo $e->getMessage();
+//     // redirection en PHP vers Google avec le message d'erreur concerné
+//     header('Location: https://www/google.fr/search?q=' .$e->getMessage());
+// }
+
 // Création d'une connexion à la BDD
-$db = new PDO('mysql:host=localhost; dbname=pizzastore', 'root', ''); 
+$db = new PDO('mysql:host=localhost; dbname=pizzastore; charset=utf8', 'root', '');
 
 var_dump($db);
+
+// on créé une requête pour récupérer les pizzas
+$query = $db->query('SELECT * FROM pizza');
+var_dump($query);
+
+// pour récupérer une seule pizza
+// ATTENTION, FETCH() déplace le curseur à l'id suivant pour la prochaine requête
+// $pizza = $query->fetch();
+// var_dump($pizza);
+
+// pour récupérer toutes les pizzas
+$pizzas = $query->fetchAll();
+var_dump($pizzas);
+
+// parcourir toutes les pizzas avec FETCHALL (nom affiché avec h1);
+
+echo '<h2>Toutes les pizzas avec FETCHALL</h2>';
+
+$queryName = $db->query('SELECT name FROM pizza');
+$pizzasName = $queryName->fetchAll();
+var_dump($pizzasName);
+
+foreach($pizzasName as $pizzaName) {
+    echo '<h3>'.$pizzaName['name'].'</h3>';
+}
+
+// parcourir toutes les pizzas avec le FETCHH uniquement
+echo '<h2>Toutes les pizzas avec FETCH uniquement</h2>';
+
+$query2 = $db->query('SELECT name FROM pizza');
+while ($pizza2 = $query2->fetch()) {
+    echo $pizza2['name'].' ; ';
+}
+
