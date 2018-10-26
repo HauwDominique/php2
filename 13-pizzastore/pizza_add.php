ed322433f4076c569a6ba160259081b24f01068b
hauw <hauw.dominique@gmail.com>
2018-10-26 12:48:25 +0200

    ajout a la bdd pizzaAd

--- 13-pizzastore/pizza_add.php
+++ 13-pizzastore/pizza_add.php
@@ -4,46 +4,67 @@ require_once(__DIR__.'/partials/header.php');?>
 
 <?php
 		// On déclare les variables pour éviter les erreurs
-		$pizza_name = null;
-        $pizza_price = null;
-        $pizza_image = null;
-        $pizza_description = null;
+		$name = null;
+        $price = null;
+        $image = null;
+        $description = null;
 
 		$message = null;
 		if (!empty($_POST)) { // Récupére les informations saisies dans le formulaire
-		    $pizza_name = $_POST['pizza_name']; //ok 
-            $pizza_price = $_POST['pizza_price']; //ok
-            $pizza_image = $_POST['pizza_image'];
-		    $pizza_description = $_POST['pizza_description'];
+		    $name = $_POST['name']; //ok 
+            $price = $_POST['price']; //ok
+            $image = $_POST['image'];
+		    $description = $_POST['description'];
 
 		    // $message = $_POST['message'];
 
 			$errors = [];
 
-		    if (empty($pizza_name)) {
-		    	$errors['pizza_name'] = 'Le nom ne doit pas être vide. <br />';
+		    if (empty($name)) {
+		    	$errors['name'] = 'Le nom ne doit pas être vide. <br />';
 		        //echo 'Le sujet ne doit pas être vide. <br />';
             }
             
-            if (empty($pizza_price)) {
-		    	$errors['pizza_price'] = 'Le prix ne doit pas être vide. <br />';
+            if (empty($price)) {
+		    	$errors['price'] = 'Le prix ne doit pas être vide. <br />';
 		        //echo 'Le sujet ne doit pas être vide. <br />';
             }
             
-            if (empty($pizza_image)) {
-		    	$errors['pizza_image'] = 'Le champ des image ne doit pas être vide. <br />';
+            if (empty($image)) {
+		    	$errors['image'] = 'Le champ des image ne doit pas être vide. <br />';
 		        //echo 'Le sujet ne doit pas être vide. <br />';
             }
             
-            if (empty($pizza_description)) {
-		    	$errors['pizza_description'] = 'Le champ description ne doit pas être vide. <br />';
+            if (empty($description)) {
+		    	$errors['description'] = 'Le champ description ne doit pas être vide. <br />';
 		        //echo 'Le sujet ne doit pas être vide. <br />';
             }
 
 		    if (empty($errors)) {
 			    echo 'Envoi de l\'ajout de la pizza';
-			}
-		}
+            }
+            
+            // S'il n'y a pas d'erreurs dans le formulaire
+            if (empty($errors)) {
+            $query = $db->prepare('
+                INSERT INTO pizza (`name`, `price`, `image`) VALUES (:name, :price, :image)
+            ');
+            $query->bindValue(':name', $name, PDO::PARAM_STR);
+            $query->bindValue(':price', $price, PDO::PARAM_STR);
+            $query->bindValue(':image', $image, PDO::PARAM_STR);
+
+            if ($query->execute()) { // On insère la pizza dans la BDD
+                $success = true;
+                // Envoyer un mail ?
+                // Logger la création de la pizza
+            }
+        }
+    }
+        
+        // si il n'y a pas d'erreur dans le formulaire
+        // if(empty($errors)) {
+        //     $query
+        // }
 	?>
 
 
@@ -52,41 +73,41 @@ require_once(__DIR__.'/partials/header.php');?>
 
     <form method="POST">
         <div class="form-group">
-            <label for="pizza_name" class='label_title'>Nom de la pizza</label>
-            <input class="form-control <?= (isset($errors['pizza_name'])) ? 'is-invalid' : ''; ?>" type="text" name="pizza_name"
-                id="pizza_name" value="<?= $pizza_name; ?>" />
-                <div class="invalid-feedback">
-		        	<?php echo (isset($errors['pizza_name'])) ? $errors['pizza_name']: ''; ?>
-		        </div>
+            <label for="name" class='label_title'>Nom de la pizza</label>
+            <input class="form-control <?= (isset($errors['name'])) ? 'is-invalid' : ''; ?>" type="text" name="name" id="name"
+                value="<?= $name; ?>" />
+            <div class="invalid-feedback">
+                <?php echo (isset($errors['name'])) ? $errors['name']: ''; ?>
+            </div>
         </div>
 
-        <label for="pizza_price" class='label_title'>Prix de la pizza</label>
-        <input class="form-control <?= (isset($errors['pizza_price'])) ? 'is-invalid' : ''; ?>" type="text" name="pizza_price"
-            id="pizza_price" value="<?= $pizza_price; ?>" />
-            <div class="invalid-feedback">
-		        	<?php echo (isset($errors['pizza_price'])) ? $errors['pizza_price']: ''; ?>
-		        </div>
+        <label for="price" class='label_title'>Prix de la pizza</label>
+        <input class="form-control <?= (isset($errors['price'])) ? 'is-invalid' : ''; ?>" type="text" name="price" id="price"
+            value="<?= $price; ?>" />
+        <div class="invalid-feedback">
+            <?php echo (isset($errors['price'])) ? $errors['price']: ''; ?>
+        </div>
 
         <div class="form-group">
-            <label for="pizza_image" class='label_title'>Image de la pizza</label>
-             <input class="form-control <?= (isset($errors['pizza_image'])) ? 'is-invalid' : ''; ?>" type="text" name="pizza_image"
-            id="pizza_image" value="<?= $pizza_image; ?>" />
-          
+            <label for="image" class='label_title'>Image de la pizza</label>
+            <input class="form-control <?= (isset($errors['image'])) ? 'is-invalid' : ''; ?>" type="text" name="image"
+                id="image" value="<?= $image; ?>" />
+
             <div class="invalid-feedback">
-		        <?php echo (isset($errors['pizza_image'])) ? $errors['pizza_image']: ''; ?>
-		    </div>
-        </div>      
-        
+                <?php echo (isset($errors['image'])) ? $errors['image']: ''; ?>
+            </div>
+        </div>
+
         <div class="form-group">
-            <label for="description_pizza" class='label_title'>Description de votre pizza</label>
+            <label for="description" class='label_title'>Description de votre pizza</label>
             <!-- <textarea class="form-control" id="description_pizza" rows="4"></textarea> -->
 
-            <textarea class="form-control <?= (isset($errors['pizza_description'])) ? 'is-invalid' : ''; ?>" 
-            name="pizza_description" id="pizza_description"><?php echo $message; ?></textarea>
-		        <div class="invalid-feedback">
-		        	<?php echo (isset($errors['pizza_description'])) ? $errors['pizza_description']: ''; ?>
-		        </div>
-            
+            <textarea class="form-control <?= (isset($errors['description'])) ? 'is-invalid' : ''; ?>" name="description"
+                id="description"><?php echo $message; ?></textarea>
+            <div class="invalid-feedback">
+                <?php echo (isset($errors['description'])) ? $errors['description']: ''; ?>
+            </div>
+
         </div>
         <div class="form-group">
             <label for="categorie" class='label_title'>Catégorie</label>
@@ -98,11 +119,9 @@ require_once(__DIR__.'/partials/header.php');?>
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
-
     </form>
-
-
 </main>
+
 
 <?php
 // Le fichier footerer.php est inclus dans la page
